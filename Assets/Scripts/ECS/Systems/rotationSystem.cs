631b99b8892c86b076f451fb66713eb56ab47359
Amel-unity <amel.negra@unity3d.com>
2020-04-02 18:32:32 +0200

    rotation system added on triangle

---
+++ Assets/Scripts/ECS/Systems/rotationSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Random = UnityEngine.Random;
+public class rotationSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+
+        float deltaTime = Time.DeltaTime;
+        Entities.WithName("Rotate")
+             .ForEach((ref Rotation rotation, in Triangle triangle) =>
+             {
+                 float rotationThisFrame = deltaTime * Random.Range(3,10);
+                 var q = quaternion.AxisAngle(new float3(0.0f, 0.0f, Random.Range(-1, 1)), rotationThisFrame);
+                 rotation.Value = math.mul(q, rotation.Value);
+
+             }).Run();
+    }
+}
