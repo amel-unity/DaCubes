c1cffaa6c24ee8da2503f5b351148caec67263bd
Ciro Continisio <ciro@unity3d.com>
2020-04-07 14:16:00 +0200

    Merged stuff

--- Assets/Scripts/ECS/Authoring/VelocityAuthoring.cs
+++ Assets/Scripts/ECS/Authoring/VelocityAuthoring.cs
@@ -1,21 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Unity.Entities;
 using Unity.Mathematics;
-using Random = UnityEngine.Random;
 
 [DisallowMultipleComponent]
 
 internal class VelocityAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
-    public float Speed = 4f;
-    public float3 MoveVector = new float3(0,0,-1);
-    public bool Touched = false;
+    public float speed = 4f;
+    public float3 moveVector = new float3(0,0,-1);
 
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        dstManager.AddComponentData(entity, new Velocity() { Speed = Speed, MoveVector = MoveVector, Touched = Touched });
+        dstManager.AddComponentData(entity, new Cube() { Speed = speed, MoveVector = moveVector });
     }
 
    
